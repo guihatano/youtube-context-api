82359f5ea58b0e4713491018354e706aca6293d7
Guiherme Y. Hatano <guilherme.hatano@libercapital.com.br>
2021-10-26 18:21:21 -0300

    remove unused imports and format with prettier

--- final/src/pages/Login.js
+++ final/src/pages/Login.js
@@ -1,9 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { Context } from '../Context/AuthContext';
+import { Context } from "../Context/AuthContext";
 
 export default function Login() {
-  const { authenticated, handleLogin } = useContext(Context);
+  const { handleLogin } = useContext(Context);
 
-  return <button type="button" onClick={handleLogin}>Entrar</button>;
+  return (
+    <button type="button" onClick={handleLogin}>
+      Entrar
+    </button>
+  );
 }
